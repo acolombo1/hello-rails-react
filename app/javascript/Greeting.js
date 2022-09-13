9f11b672491db383ba34ef9203bc308724d55c26
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-09-12 22:37:56 -0300

    Add app and greeting component

---
+++ app/javascript/Greeting.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Greeting() {
+  return (
+    <h1>
+      Will display here a greeting got from the api
+    </h1>
+  );
+}
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,9 +5,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import './index.css';
+import Greeting from './Greeting';
 
 function App() {
-  return (<h1>Hi World!</h1>);
+  return (
+    <>
+      <h1>Hello, this is the greeting App.</h1>
+      <Greeting />
+    </>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
